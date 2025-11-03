0e2b7858697959adf044dd58353c7ee55f26b3b7
palakgupta5412 <palakgupta425@gmail.com>
2025-11-04 01:03:34 +0530

    bcypt and jwt

---
+++ src/models/user.model.js
@@ -0,0 +1,96 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+const userSchema = new mongoose.Schema({
+    username : {
+        type : String,
+        required : true,
+        unique : true,
+        trim : true,
+        lowercase : true,
+        index : true               // Adding index for faster search
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true,
+        trim : true,
+        lowercase : true,
+    }, 
+    fullName : {
+        type : String,
+        required : true,
+        trim : true,
+        index : true
+    },
+    avatar : {
+        type : String,                    // URL of the avatar image stored in cloudinary or any other service
+        required : true,
+        message : "URL of the avatar image"
+    },
+    coverImage : {
+        type : String,                   
+        required : false,
+        message : "URL of the cover image"
+    },
+    watchHistory : [{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'Video'                    // Referencing Video model
+    }],
+    password : {
+        type : String,
+        required : [true , "Password is required"],
+    },
+    refreshToken:{
+        type : String,
+    }
+}, { timestamps: true });
+
+// Pre-save hook to run a function before saving a user : 
+// For example, hashing password before saving
+// Dont use arrow function here to access 'this' because arrow functions do not bind 'this'
+
+userSchema.pre('save', function(next){
+    
+    // Check if password is modified or not , if not modified we move further since this pre hook will be called on every save
+    if(!this.isModified('password')) return next();
+    // Hash the password using bcrypt
+    this.password = bcrypt.hash(this.password, 10);
+    next();
+})
+
+//In user Schemas we can add methods to compare password during login
+// userSchema ke andar methods add kr skte , isPasswordMatch and generateAccessToken are name of fns we are adding to schema 
+
+userSchema.methods.isPasswordMatch = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function(){
+    jwt.sign({
+        _id : this._id,
+        username : this.username,
+        email : this.email,
+        fullName : this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    jwt.sign(
+        {
+            _id : this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model('User', userSchema);
---
+++ src/models/video.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new mongoose.Schema({
+    videoFile : {
+        type : String,                    // URL of the video file stored in cloudinary or any other service
+        required : true,
+        message : "URL of the video file"
+    },
+    thumbnail : {
+        type : String,                    // URL of the thumbnail image stored in cloudinary or any other service
+        required : true,
+        message : "URL of the thumbnail image"
+    },
+    title : {
+        type : String,
+        required : true
+    },
+    description : {
+        type : String,
+        required : true
+    },
+    duration : {
+        type : Number,                    // Duration in seconds obrtained from cloudinary or any other service
+        required : true
+    },
+    views : {
+        type : Number,
+        default : 0
+    },
+    isPublished : {
+        type : Boolean,
+        default : true
+    }, 
+    owner : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'User',                    // Referencing User model
+        required : true
+    }, 
+
+}, { timestamps: true });
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model('Video', videoSchema);
